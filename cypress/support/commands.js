2bb97a081d4d4beceb5326cba38c2560e638585b
novilla7 <nopilla7@gmail.com>
2024-06-04 07:57:47 +0700

    API Authentication Commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -97,3 +97,23 @@ Cypress.Commands.add('Purchase2', (fillAmount2) => {
 Cypress.Commands.add('Purchase3', (selectRadio) => {
     cy.get(selectRadio).click()
 })
+
+
+
+//// COMMANDS API
+
+Cypress.Commands.add('loginViaAPI', (
+    email = Cypress.env('userEmail'),
+    password = Cypress.env('userPassword')
+ ) => {
+
+    cy.request('POST', `${Cypress.env('apiUrl')}/users/login`, {
+        username: email,
+        password,
+    }).then((response) => {
+        cy.setCookie('sessionId', response.body.sessionId)
+        cy.setCookie('userId', response.body.userId)
+        cy.setCookie('userName', response.body.userName)
+        cy.visit('/#!/main')
+    })
+})
